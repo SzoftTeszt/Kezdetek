607a803d8d2cae5b4eafa17fa8e997a8a4aa944a
Attila Jager <jagerattila@gmail.com>
2023-05-18 08:58:03 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[
+        FormsModule
+      ],
       declarations: [
         AppComponent
+      
       ],
     }).compileComponents();
   });
@@ -16,16 +21,75 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Kezdetek'`, () => {
+  it('Üdvözlés ellenőrzése',()=>{
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Kezdetek');
+    const appElements= fixture.nativeElement;
+    const h1 = appElements.querySelector('h1');
+    expect(h1.textContent).toContain('Szevasz');
   });
 
-  it('should render title', () => {
+
+  it('Username ellenőrzés',()=>{
     const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const appElements= fixture.nativeElement;
+    
+    const h2= appElements.querySelector('h2');
+    app.userName="Cica";
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Kezdetek app is running!');
+    expect(h2.textContent).toContain(app.userName);
+
   });
+
+  it('Template=>Controller Bind',()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const appElements= fixture.nativeElement;
+  
+    const adatInput = appElements.querySelector('#adat');
+    adatInput.value="kreativítás";
+    fixture.detectChanges();
+    adatInput.dispatchEvent(new Event('input'));
+    expect(adatInput.value).toEqual(app.adat);
+
+  })
+
+  it('Controller=>Template Bind',fakeAsync(
+    ()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const appElements= fixture.nativeElement;
+
+    const adatInput = appElements.querySelector('#adat');
+    app.adat="nyuszika";
+    fixture.detectChanges();
+    fixture.whenStable().then( ()=>
+      {
+        expect(app.adat).toEqual(adatInput.value);
+      }
+    )
+    }))
+  
+
+  it('Empty',()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const appElements= fixture.nativeElement;
+  
+  })
+
+
+  // it(`should have as title 'Kezdetek'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('Kezdetek');
+  // });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('.content span')?.textContent).toContain('Kezdetek app is running!');
+  // });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Kezdetek';
+  title:string = 'Kezdetek';
+  isAdmin:boolean=true;
+  // MVC MOdel View Controller
+  userName:string;
+  oszlopok=["id","name","type"];
+
+  ujAllat:any={};
+  
+  adat:any="";
+  adatok:any=[];
+
+  addAdat(vmi:any){
+    this.adatok.push(vmi);
+    console.log("adatok hozzáadva");
+  }
+
+  allatok:any=[
+    {id:1, name:"Negro", type:"kutyus"},
+    {id:2, name:"Inci", type:"cica"},
+    {id:3, name:"Csipa", type:"kutyus"}
+  ];
+  
+  addAnimal(allat:any){
+    this.allatok.push(allat);
+  }
+  constructor(){
+    this.userName="Julcsika";
+  }
 }
